9db87587d140ec47cf77bd40c5c6e21420ac7d71
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-05-03 20:01:51 +0530

    changed 3 files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Articles from "./components/Articles";
+import User from "./components/User";
 function App() {
-  return <div className="App">Hey</div>;
+  return (
+    <div className="App">
+      <header>
+        <h1>React Skeletons</h1>
+      </header>
+
+      <div className="content">
+        <Articles></Articles>
+        <User></User>
+      </div>
+    </div>
+  );
 }
 
 export default App;
