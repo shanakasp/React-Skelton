726a99b882ceeb88b29e9485c1f40558340f6286
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-05-04 09:32:36 +0530

    modified 3 files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import Articles from "./components/Articles";
-import User from "./components/User";
+import { useEffect, useState } from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <div className="App">
       <header>
@@ -8,8 +20,41 @@ function App() {
       </header>
 
       <div className="content">
-        <Articles></Articles>
-        <User></User>
+        {loading ? (
+          <>
+            <Skeleton />
+            <Skeleton count={3} />
+          </>
+        ) : (
+          <>
+            <h2>Menus</h2>
+            <ul>
+              <li>
+                <a href="#">Menu Item 1</a>
+              </li>
+              <li>
+                <a href="#">Menu Item 2</a>
+              </li>
+              <li>
+                <a href="#">Menu Item 3</a>
+              </li>
+            </ul>
+            <h2>Sub Topics</h2>
+            <ol>
+              <li>Sub Topic 1</li>
+              <li>Sub Topic 2</li>
+              <li>Sub Topic 3</li>
+            </ol>
+            <h2>Lorem Ipsum</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+              facilisi. Sed vehicula ipsum nec odio ullamcorper, at auctor
+              tortor suscipit. Duis vehicula justo eu dui lacinia, a scelerisque
+              tortor vestibulum. Donec lobortis purus at leo sodales, vel congue
+              dui dapibus.
+            </p>
+          </>
+        )}
       </div>
     </div>
   );
